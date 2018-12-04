b4197ddf56f06cad0c2d0c2d95550e27da793a00
Gerd Haverbeck <gerd.haverbeck@posteo.de>
2018-12-04 22:48:52 +0100

    Generieren der UI

--- js/myhome-app.js
+++ js/myhome-app.js
@@ -10,7 +10,7 @@ console.log(config);
 
 
 for(let i=0; i<config.floors.length; i++) {
-	let floor = config.floors[i]) 
+	let floor = config.floors[i]);
 	for(let j=0; j<floor.rooms.length; j++) {
 		let room = floor.rooms[j];
 		let device = "";
